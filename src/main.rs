cfe5c50b8db992d5e0aff01059a03f5ad48b8e0f
makinzm <nozomi.maki.da@gmail.com>
2024-12-19 00:23:59 +0900

    feat: load test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use goose::prelude::*;
+use goose_eggs::{validate_and_load_static_assets, Validate};
 
 async fn loadtest_index(user: &mut GooseUser) -> TransactionResult {
-    let _goose_metrics = user.get("").await?;
+    // リクエストを送信
+    let goose = user.get("/rust-wasm-github/").await?;
+
+    // 検証条件を定義
+    let validate = &Validate::builder()
+        .status(200)
+        .text("makinzm HP")
+        .build();
+
+    // 検証と静的リソースのロード
+    validate_and_load_static_assets(user, goose, validate).await?;
 
     Ok(())
 }
@@ -9,8 +20,9 @@ async fn loadtest_index(user: &mut GooseUser) -> TransactionResult {
 #[tokio::main]
 async fn main() -> Result<(), GooseError> {
     GooseAttack::initialize()?
-        .register_scenario(scenario!("LoadtestTransactions")
-            .register_transaction(transaction!(loadtest_index))
+        .register_scenario(
+            scenario!("LoadtestTransactions")
+                .register_transaction(transaction!(loadtest_index))
         )
         .execute()
         .await?;
